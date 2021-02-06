06ee31866e495f7ae6f567227c6b61f934c51ace
David <destevezbreso@gmail.com>
2021-02-06 00:09:39 -0500

    addcontext to logSession  persistent 4 all loggers

--- example/test-proxy.js
+++ example/test-proxy.js
@@ -1,17 +1,19 @@
 const { LogSession } = require('../index');
 
-const generic = LogSession.getLogger();
+const generic = LogSession
+  .addContext({ generic: 'qwrty' })
+  .getLogger();
 generic.info('log some stuff w/ generic logger');
-
 
 const logger = LogSession.getLogger('TEST:PROXY');
 
 logger.info('start tests');
+logger.debug('add context for test1');
+LogSession.addContext({ level1Time: 'level1Time' });
 logger.debug('loading test 1 from proxy');
 require('./test1');
 
+logger.debug('add context for test2');
+LogSession.addContext({ level2Time: 'level2Time' });
 logger.debug('loading test 2 from proxy');
 require('./test2');
-
-
-console.log(LogSession);
